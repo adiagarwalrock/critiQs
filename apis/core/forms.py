6bc31af66e292d2cf5685461adbc16277623bc96
Aditya Agarwal <aditya.ag1234@gmail.com>
2023-04-26 03:18:41 -0400

    User Signup Working

--- apis/core/forms.py
+++ apis/core/forms.py
@@ -1,6 +1,10 @@
+
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 from api.models import Comment
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class CommentForm(forms.ModelForm):
@@ -19,3 +23,42 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body', 'content_id', 'user']
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username',
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'autocomplete': 'new-password',
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm Password',
+                'autocomplete': 'new-password',
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
