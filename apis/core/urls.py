e4920b7b3b233448e7c987a65629a1748f77f930
Aditya Agarwal <aditya.ag1234@gmail.com>
2023-04-27 04:26:55 -0400

    added tv series page and sample dictionary for ref

--- apis/core/urls.py
+++ apis/core/urls.py
@@ -5,13 +5,17 @@ from django.urls import include, path
 from core.views import (
     Home,
     MovieDetailView,
-    SignUpView
+    SignUpView,
+    TvSeriesDetailView,
 )
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("movie-detail/<int:movie_id>/",
          MovieDetailView.as_view(), name="movie_details"),
+
+    path("series-detail/<int:series_id>",
+         TvSeriesDetailView.as_view(), name="series_details"),
 
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
